502d970f88e1c12a02b911553888305e3d0f0603
Lukas Hodel <lukas.hodel@swisscom.com>
2022-03-10 22:19:40 +0100

    Push back to git

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,23 @@ cd /vuepress
 cp -r /github/workspace remote
 yarn run remote:build
 ls -la ./remote/.vuepress/dist
+
+cp -r ./remote/.vuepress/dist /github/workspace/page
+
+cd /github/workspace
+remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+git init && \
+git config user.name "bbzbl-page-action" && \
+git config user.email "bbzbl-page-action@users.noreply.github.com" && \
+git add . && \
+git status && \
+curr_branch="$(git rev-parse --abbrev-ref HEAD)" && \
+commit_msg="${COMMIT_MSG:-'Vuepress Action Build'}" && \
+git commit -m "${commit_msg}" 
+
+if [ "$NO_FORCE_PUSH" = true ]
+then 
+  git push $remote_repo ${curr_branch}:${PUBLISH_TO_BRANCH}
+else
+  git push --force $remote_repo ${curr_branch}:${PUBLISH_TO_BRANCH}
+fi
